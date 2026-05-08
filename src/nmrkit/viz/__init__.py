0324e9edf1bb9e6bac9e3993addd20d6f40e63e6
Bart Rosa <contact@bartrosa.dev>
2026-05-08 11:35:52 +0200

    feat: add visualization module with plotting functions for HSQC overlays and kinetic curves; include summary generation for NMR time series

---
+++ src/nmrkit/viz/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: Apache-2.0
+
+"""Visualization helpers."""
+
+from nmrkit.viz.plot import plot_hsqc_with_clusters, plot_kinetic_curves, plot_summary
+
+__all__ = [
+    "plot_hsqc_with_clusters",
+    "plot_kinetic_curves",
+    "plot_summary",
+]
